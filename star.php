6fc3fb455ac657d469d8ed0b21e998c415895177
alidf1372 <alidf1372@gmail.com>
2022-03-26 15:55:57 +0430

    add file in projects

---
+++ star.php
@@ -0,0 +1,25 @@
+<?php
+
+
+echo '<form name="form" action="" method="post">
+  <input type="text" name="subject" id="subject">
+  <input type="submit" value="send">
+</form>';
+
+if (!empty($_POST)) {
+    $num = $_POST['subject'];
+    function star($num) {
+        $space = floor($num/2);
+        $star = ceil($num/2);
+        for($i=0;$i<$num;$i++) {
+            for($j=$space-1;$j>=$i;$j--) {
+                echo "-&nbsp&nbsp&nbsp";
+            }
+            for($k=1;$k<$i;$k++) {
+
+            }
+            echo "<br>";
+        }
+    }
+    star($num);
+}
